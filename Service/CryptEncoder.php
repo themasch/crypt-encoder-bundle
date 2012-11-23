895e4436789908ff4b84725bb73bae5e6e8dc03a
Mark Schmale <masch@masch.it>
2012-11-23 03:22:23 +0100

    initial commit

---
+++ Service/CryptEncoder.php
@@ -0,0 +1,19 @@
+<?php
+namespace maschit\CryptEncoderBundle\Service;
+
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+
+class CryptEncoder implements PasswordEncoderInterface
+{
+
+    public function encodePassword($raw, $salt)
+    {
+        return crypt($raw, $salt);
+    }
+
+    public function isPasswordValid($encoded, $raw, $salt)
+    {
+        return $encoded === $this->encodePassword($raw, $salt);
+    }
+
+}
---
+++ maschitCryptEncoderBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace maschit\CryptEncoderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class maschitCryptEncoderBundle extends Bundle
+{
+}
