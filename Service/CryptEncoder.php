e2f49d4654a3db1e56c0cf81b52be73ea83a09e3
Mark Schmale <masch@masch.it>
2012-12-03 17:53:34 +0100

    fixed requirements

--- Service/CryptEncoder.php
+++ Service/CryptEncoder.php
@@ -6,14 +6,14 @@ use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 class CryptEncoder implements PasswordEncoderInterface
 {
 
-    public function encodePassword($raw, $salt)
-    {
-        return crypt($raw, $salt);
-    }
+  public function encodePassword($raw, $salt)
+  {
+    return crypt($raw, $salt);
+  }
 
-    public function isPasswordValid($encoded, $raw, $salt)
-    {
-        return $encoded === $this->encodePassword($raw, $salt);
-    }
+  public function isPasswordValid($encoded, $raw, $salt)
+  {
+    return $encoded === $this->encodePassword($raw, $salt);
+  }
 
 }
